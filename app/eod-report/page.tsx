c24d6a247ce241863bf926521aa94c3c78c8de8d
Garvin Tang <garvin9006@gmail.com>
2025-11-25 21:46:39 +1100

    added POS report delete confirmation

--- app/eod-report/page.tsx
+++ app/eod-report/page.tsx
@@ -13,6 +13,8 @@ import {
 } from "@/state/moneyAtoms";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { CircleMinus, Trash2 } from "lucide-react";
+
 
 interface SalesStateEditorProps {
   idx: number;
@@ -24,7 +26,17 @@ function SalesStateEditor({ idx, salesAtom, onDelete }: SalesStateEditorProps) {
   const [sales, setSales] = useAtom(salesAtom);
   return (
     <div className="flex flex-col order mb-1 space-y-2">
-      <p className="font-semibold">POS {idx + 1}</p>
+      <div className="flex items-center justify-between">
+        <p className="font-semibold">POS {idx + 1}</p>
+        {idx != 0 && (
+          <button
+            onClick={onDelete}
+            aria-label="Delete sales entry"
+          >
+            <Trash2 size={24} color="red" />
+          </button>
+        )}
+      </div>
       <label className="justify-between flex items-center">
         Net Sales:
         <input
@@ -58,17 +70,6 @@ function SalesStateEditor({ idx, salesAtom, onDelete }: SalesStateEditorProps) {
           className="border p-1 w-24"
         />
       </label>
-      {idx != 0 ? (
-        <button
-          onClick={onDelete}
-          className="px-2 py-1 bg-red-600 text-white rounded"
-          aria-label="Delete sales entry"
-        >
-          X
-        </button>
-      ) : (
-        <></>
-      )}
     </div>
   );
 }
@@ -84,10 +85,11 @@ export default function EODReport() {
     setAtoms((prev) => [...prev, newAtom]);
   };
 
-  const handleDelete = (index: number) => {
-    console.log(index);
+const handleDelete = (index: number) => {
+  if (confirm("Are you sure you want to delete this sales entry?")) {
     setAtoms((prev) => prev.filter((_, i) => i !== index));
-  };
+  }
+};
 
   return (
     <div className="flex min-h-screen items-center justify-center ">
