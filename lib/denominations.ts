a162a511c729e697520c11de24b80c7a950a2c0a
Garvin Tang <garvin9006@gmail.com>
2025-12-04 14:20:42 +1100

    changed dollar roll coin count from 25 to 20

--- lib/denominations.ts
+++ lib/denominations.ts
@@ -6,7 +6,7 @@ export const denominations = [
   { label: "$10", value: 10, rollSize: undefined },
   { label: "$5", value: 5, rollSize: undefined },
   { label: "$2", value: 2, rollSize: 25 },
-  { label: "$1", value: 1, rollSize: 25 },
+  { label: "$1", value: 1, rollSize: 20 },
   { label: "50c", value: 0.5, rollSize: 20 },
   { label: "20c", value: 0.2, rollSize: 20 },
   { label: "10c", value: 0.1, rollSize: 40 },
