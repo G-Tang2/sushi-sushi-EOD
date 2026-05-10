64e641e75da6019755469202f330c78cdc3394f0
Garvin Tang <garvin9006@gmail.com>
2026-05-10 11:29:27 +1000

    changed loading screen title to be loading icon

--- app/history/loading.tsx
+++ app/history/loading.tsx
@@ -4,11 +4,13 @@ export default function Loading() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center sm:items-start">
-        <h1 className="my-8 text-4xl font-bold">History</h1>
+        <div className="flex flex-col items-center w-full max-w-xs gap-2 my-9">
+            <LoaderCircle size={32} className="animate-spin text-zinc-500" />
+        </div>
         <div className="flex flex-col gap-2 bg-slate-50 w-sm py-4 px-4 rounded-2xl">
           <div className="flex flex-col items-center justify-center gap-2">
             <LoaderCircle size={32} className="animate-spin text-zinc-500" />
-            <div>Loading reports...</div>
+            <div>Loading reports...</div>   
           </div>
         </div>
       </main>
